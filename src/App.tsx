a5888fcc4e8dc209f3cc84a11ef3e749d006ff36
Asnan Ashfaq <65377376+AsnanAshfaq@users.noreply.github.com>
2021-04-22 01:09:07 +0500

    just committing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { FunctionComponent } from "react";
 import Header from "./components/Header";
-function App() {
+const App: FunctionComponent = () => {
   return <Header />;
-}
+};
 
 export default App;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,13 @@
 import React, { FunctionComponent } from "react";
 import * as ReactBootStrap from "react-bootstrap";
 import "../App.css";
-import search from '../assets/Images/search.png'
-import newsfeed from '../assets/Images/newsfeed.png'
+import search from "../assets/Images/search.png";
+import newsfeed from "../assets/Images/newsfeed.png";
 import logout from "../assets/Images/logout.png";
 import projects from "../assets/Images/projects.png";
-import workshop from '../assets/Images/workshop.png'
-import bell from '../assets/Images/bell.png'
-import hackathon from '../assets/Images/hackathon.png'
+import workshop from "../assets/Images/workshop.png";
+import bell from "../assets/Images/bell.png";
+import hackathon from "../assets/Images/hackathon.png";
 
 const Header: FunctionComponent = () => {
   return (
@@ -25,79 +25,44 @@ const Header: FunctionComponent = () => {
             <ReactBootStrap.Form inline>
               <ReactBootStrap.FormControl type="text" placeholder="Search" />
               <ReactBootStrap.Navbar.Brand href="searchicon">
-                <img
-                  src={search}
-                  height="20"
-                  width="20"
-                  alt="searchicon"
-                />
+                <img src={search} height="20" width="20" alt="searchicon" />
               </ReactBootStrap.Navbar.Brand>
             </ReactBootStrap.Form>
           </ReactBootStrap.Nav>
 
           <ReactBootStrap.Nav className="feedlogo">
             <ReactBootStrap.Navbar.Brand href="feed">
-              <img
-                src={newsfeed}
-                width="35"
-                height="35"
-                alt="feedlogo"
-              />
+              <img src={newsfeed} width="35" height="35" alt="feedlogo" />
             </ReactBootStrap.Navbar.Brand>
           </ReactBootStrap.Nav>
 
           <ReactBootStrap.Nav className="hackathonlogo">
             <ReactBootStrap.Navbar.Brand href="hackathons">
-              <img
-                src={hackathon}
-                width="35"
-                height="35"
-                alt="hackathonlogo"
-              />
+              <img src={hackathon} width="35" height="35" alt="hackathonlogo" />
             </ReactBootStrap.Navbar.Brand>
           </ReactBootStrap.Nav>
 
           <ReactBootStrap.Nav className="projectslogo">
             <ReactBootStrap.Navbar.Brand href="projects">
-              <img
-                src={projects}
-                width="35"
-                height="35"
-                alt="projectslogo"
-              />
+              <img src={projects} width="35" height="35" alt="projectslogo" />
             </ReactBootStrap.Navbar.Brand>
           </ReactBootStrap.Nav>
 
           <ReactBootStrap.Nav className="workshopslogo">
             <ReactBootStrap.Navbar.Brand href="workshops">
-              <img
-                src={workshop}
-                width="35"
-                height="35"
-                alt="workshopslogo"
-              />
+              <img src={workshop} width="35" height="35" alt="workshopslogo" />
             </ReactBootStrap.Navbar.Brand>
           </ReactBootStrap.Nav>
 
           <ReactBootStrap.Nav className="belllogo">
             <ReactBootStrap.Navbar.Brand href="notification">
-              <img
-                src={bell}
-                width="35"
-                height="35"
-                alt="notificationlogo"
-              />
+              <img src={bell} width="35" height="35" alt="notificationlogo" />
             </ReactBootStrap.Navbar.Brand>
           </ReactBootStrap.Nav>
 
           <ReactBootStrap.Nav className="logoutlogo">
             <ReactBootStrap.Navbar.Brand href="logout">
-              <img
-                src={logout}
-                width="35"
-                height="35"
-                alt="logoutlogo"
-              />
+              <img src={logout} width="35" height="35" alt="logoutlogo" />
             </ReactBootStrap.Navbar.Brand>
           </ReactBootStrap.Nav>
 
